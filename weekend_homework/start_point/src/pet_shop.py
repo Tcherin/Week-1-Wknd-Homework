77c8394b5ac1adc65bb64fdd661cfd9814b79abc
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-03-28 09:48:45 +0100

    submitting weekend homework

--- weekend_homework/start_point/src/pet_shop.py
+++ weekend_homework/start_point/src/pet_shop.py
@@ -1,13 +1,70 @@
 # WRITE YOUR FUNCTIONS HERE
 
 def get_pet_shop_name(pet_shop):
-    
-        return name
+        return pet_shop["name"]
 
-def total_cash(pet_shop):
-    for sum in pet_shop:
-       if get_total_cash == 1000:
-            return sum
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, num1):
+        pet_shop["admin"]["total_cash"] += num1
+
+def get_pets_sold(pet_shop):
+       return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, num2):
+        pet_shop["admin"]["pets_sold"] += num2
+
+def get_stock_count(pet_shop):
+        stock_total = len(pet_shop["pets"])
+        return stock_total
+
+def get_pets_by_breed(pet_shop, breed):
+      breed_list = []
+      for pet_breed in pet_shop["pets"]:
+        if pet_breed["breed"] == breed:
+                breed_list.append(pet_breed)
+      return breed_list
+
+def find_pet_by_name(pet_shop, name):
+        for pet in pet_shop["pets"]:
+            if pet["name"] == name:
+                return pet
+
+def remove_pet_by_name(pet_shop, name):
+        pet_shop["pets"].remove(find_pet_by_name(pet_shop, name))
+
+def add_pet_to_stock(pet_shop, pet):
+        pet_shop["pets"].append(pet)
+
+def get_customer_cash(customer):
+        return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+        customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+        return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet):
+        customer["pets"].append(pet)
+
+def customer_can_afford_pet(customer, pet):
+        if customer["cash"] >= pet["price"]:
+                return True
+        else:
+                return False
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+        if pet != None and customer_can_afford_pet(customer, pet):
+                remove_pet_by_name(pet_shop, pet["name"])
+                add_pet_to_customer(customer, pet)
+                remove_customer_cash(customer, pet["price"])
+                add_or_remove_cash(pet_shop, pet["price"])
+                increase_pets_sold(pet_shop, 1)
+                
+          
+        
 
 
 
--- weekend_homework/start_point/tests/pet_shop_test.py
+++ weekend_homework/start_point/tests/pet_shop_test.py
@@ -79,60 +79,60 @@ class TestPetShop(unittest.TestCase):
         name = get_pet_shop_name(self.cc_pet_shop)
         self.assertEqual("Camelot of Pets", name)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_add_or_remove_cash__add(self):
         add_or_remove_cash(self.cc_pet_shop,10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1010, cash)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_add_or_remove_cash__remove(self):
         add_or_remove_cash(self.cc_pet_shop,-10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(990, cash)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_pets_sold(self):
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_increase_pets_sold(self):
         increase_pets_sold(self.cc_pet_shop,2)
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_stock_count(self):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
         self.assertEqual(2, len(pets))
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__not_found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
         self.assertEqual(0, len(pets))
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_find_pet_by_name__returns_pet(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Arthur")
         self.assertEqual("Arthur", pet["name"])
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_find_pet_by_name__returns_None(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Fred")
         self.assertIsNone(pet)
 
-    @unittest.skip("delete this line to run the test")
+    #@unittest.skip("delete this line to run the test")
     def test_remove_pet_by_name(self):
         remove_pet_by_name(self.cc_pet_shop, "Arthur")
         pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
@@ -167,6 +167,7 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(1, get_customer_pet_count(customer))
 
     # --- OPTIONAL ---
+    # an integration test is a test that features several functions involved in the same task
 
     @unittest.skip("delete this line to run the test")
     def test_customer_can_afford_pet__sufficient_funds(self):
@@ -188,7 +189,7 @@ class TestPetShop(unittest.TestCase):
 
     # These are 'integration' tests so we want multiple asserts.
     # If one fails the entire test should fail
-    #
+    ## an integration test is a test that features several functions involved in the same task
     @unittest.skip("delete this line to run the test")
     def test_sell_pet_to_customer__pet_found(self):
         customer = self.customers[0]
